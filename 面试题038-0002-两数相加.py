1bac9f81f1c50f9682e79d80a48a68d6153477eb
chencf3 <132070041+chencf3@users.noreply.github.com>
2024-01-21 16:23:16 +0800

    Add files via upload

---
+++ 面试题038-0002-两数相加.py
@@ -0,0 +1,56 @@
+'''
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例 1：
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+
+示例 2：
+输入：l1 = [0], l2 = [0]
+输出：[0]
+
+示例 3：
+输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+输出：[8,9,9,9,0,0,0,1]
+
+提示：
+每个链表中的节点数在范围 [1, 100] 内
+0 <= Node.val <= 9
+题目数据保证列表表示的数字不含前导零
+'''
+
+
+### 方法一：
+# 时间复杂度：O(n)
+# 空间复杂度：O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head = ListNode()
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                x = l1.val
+                l1 = l1.next
+            else:
+                x = 0
+
+            if l2:
+                y = l2.val
+                l2 = l2.next
+            else:
+                y = 0
+
+            cur.val = (x + y + carry) % 10
+            carry = (x + y + carry) // 10
+            if l1 or l2 or carry:
+                cur.next = ListNode()
+                cur = cur.next
+        return head
